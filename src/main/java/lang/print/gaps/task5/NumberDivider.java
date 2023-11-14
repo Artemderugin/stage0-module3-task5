e0c995fcfbd398d4fe16a3cd57331976235fbad7
Artemderugin <artemderugin1987@gmail.com>
2023-11-14 17:10:07 +0300

    fix

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
 
-        System.out.println(toBeDivided / 5);
+        System.out.println(5 / toBeDivided);
     }
 }
